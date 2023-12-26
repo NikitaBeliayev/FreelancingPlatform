ecc449650c26734d7fd7c16030db26bc64043a36
vetelkin.aleksey <vetelkin.aleksey@student.ehu.lt>
2023-12-26 17:25:04 +0000

    Resolve "Implement global exception handler/middleware"

---
+++ Application/Models/ApiResponse.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Application.Models
+{
+	public class ApiResponse<T>
+	{
+		public T? Data { get; set; }
+		public bool Success { get; set; }
+		public string? Message { get; set; }
+		public List<string>? Errors { get; set; }
+	}
+}
